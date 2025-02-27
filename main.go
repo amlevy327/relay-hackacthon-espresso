5a602091c3ad38bac6b6581f3cc0727c164659d7
JEAN GAL <jgatbkk@gmail.com>
2025-02-27 10:33:51 -0500

    refactor transactions fetching code

--- main.go
+++ main.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"log"
-	"math/big"
 	"os"
 	"time"
 
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
 type Config struct {
-	HotShotURL      string        `json:"hotshot_url"`
-	ChainID         uint64        `json:"chain_id"`
-	PollingInterval time.Duration `json:"polling_interval"`
 	CaffNodeURL     string        `json:"caff_node_url"`
+	PollingInterval time.Duration `json:"polling_interval"`
+	Value           int           `json:"value"`
+	From            string        `json:"from"`
 }
 
 func loadConfig() Config {
@@ -43,6 +40,7 @@ func main() {
 	defer ticker.Stop()
 
 	client, err := ethclient.Dial(cfg.CaffNodeURL)
+
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
@@ -50,54 +48,6 @@ func main() {
 	var lastBlockNumber uint64
 
 	for range ticker.C {
-		transactions(cfg, client, &lastBlockNumber)
+		searchLatestTransactions(cfg, client, &lastBlockNumber)
 	}
-}
-
-func transactions(cfg Config, client *ethclient.Client, lastBlockNumber *uint64) {
-	blockNumber, err := client.BlockNumber(context.Background())
-	if err != nil {
-		log.Printf("Failed to get the latest block number: %v", err)
-		return
-	}
-
-	if blockNumber == *lastBlockNumber {
-		return // Skip if we've already seen this block
-	}
-
-	block, err := client.BlockByNumber(context.Background(), big.NewInt(int64(blockNumber)))
-	if err != nil {
-		log.Printf("Failed to get block: %v", err)
-		return
-	}
-
-	fmt.Printf("Latest Block Number: %d\n", blockNumber)
-	fmt.Println("Transactions in the Latest Block:")
-
-	for i, tx := range block.Transactions() {
-		if i == 0 {
-			continue
-		}
-		printTransactionDetails(tx)
-	}
-
-	*lastBlockNumber = blockNumber
-}
-
-func printTransactionDetails(tx *types.Transaction) {
-	msg, err := types.Sender(types.LatestSignerForChainID(tx.ChainId()), tx)
-	if err != nil {
-		log.Printf("Failed to get sender for transaction %s: %v", tx.Hash().Hex(), err)
-		return
-	}
-
-	fmt.Printf("Transaction Hash: %s\n", tx.Hash().Hex())
-	fmt.Printf("  Value: %d\n", tx.Value().Int64())
-	fmt.Printf("  From: %s\n", msg.Hex())
-	if tx.To() != nil {
-		fmt.Printf("  To: %s\n", tx.To().Hex())
-	} else {
-		fmt.Println("  To: Contract Creation")
-	}
-	fmt.Println("---------------------------")
 }
